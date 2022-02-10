f8a6735bcf83afa652602aed0ef0dff4111a766c
JacobJenness <jenne077@morris.umn.edu>
2022-02-09 20:24:00 -0600

    Finish Lab 3
    NOTE: The player movement is not tied to the frame rate. When multiplying the movement speed by `Time.deltaTime` it did not allow movement unless the player was in the air for some reason.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D mainRigidBody;
     [SerializeField] private SpriteRenderer mainSpriteRenderer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float moveSpeed;
+    
     void Update()
     {
-       // mainRigidBody.AddForce(new Vector2(-moveSpeed * Time.deltaTime, 0));
+        // For some reason, when the moveSpeed is multiplied by Time.deltaTime,
+        // the player does not move in the x direction unless they are in the air.
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            mainSpriteRenderer.flipX = true;
+            //mainRigidBody.AddForce(new Vector2(-moveSpeed * Time.deltaTime, 0));
+            mainRigidBody.AddForce(new Vector2(-moveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            mainSpriteRenderer.flipX = false;
+            //mainRigidBody.AddForce(new Vector2(moveSpeed * Time.deltaTime, 0));
+            mainRigidBody.AddForce(new Vector2(moveSpeed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        {
+            mainRigidBody.AddForce(new Vector2(0, 200));
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Unity Lab 3
+This repo stores the files for Unity Lab 3
+Written and developed by Jacob Jenness and Elena Lam
