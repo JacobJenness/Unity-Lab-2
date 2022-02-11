bac1f8932765947a08ec15e8a269fbdb3a75ed22
JacobJenness <jenne077@morris.umn.edu>
2022-02-11 12:52:43 -0600

    Add Lab 4 files

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -9,12 +9,14 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Button openCreditsButton;
     [SerializeField] private Button closeCreditsButton;
     [SerializeField] private Button platformerButton;
+    [SerializeField] private Button threeDSpawnButton;
     [SerializeField] private GameObject creditsUI;
 
     void Awake()
     {
         startButton.onClick.AddListener(() => LoadingScreen.LoadScene("GameScene"));
         platformerButton.onClick.AddListener(() => LoadingScreen.LoadScene("NotQuitePlatformer"));
+        threeDSpawnButton.onClick.AddListener(() => LoadingScreen.LoadScene("3DSpawn"));
         openCreditsButton.onClick.AddListener(() => { creditsUI.SetActive(true); });
         closeCreditsButton.onClick.AddListener(() => { creditsUI.SetActive(false); });
     }
