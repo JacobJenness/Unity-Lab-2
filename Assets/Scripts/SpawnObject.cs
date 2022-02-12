b66373659b427b64f952fcdc10ff28bc36d74b61
JacobJenness <jenne077@morris.umn.edu>
2022-02-12 13:37:42 -0600

    Finish Lab 4

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnObject : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,20 @@ public class SpawnObject : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetColor(Color c)
+    {
+        meshRenderer.material.SetColor("_Color", c);
+    }
+
+    private float lastHitTime = 0;
+    public void OnCollisionEnter(Collision collision) {
+        if((Time.time - lastHitTime) > 0.25f) {
+            lastHitTime = Time.time;
+            audioSource.volume = Mathf.InverseLerp(0,10,collision.relativeVelocity.magnitude);
+            audioSource.pitch = Random.Range(0.75f, 1.25f);
+            audioSource.Play();
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Unity Lab 3
-This repo stores the files for Unity Lab 3
+# Unity Lab 4
+This repo stores the files for Unity Lab's 1, 2, 3, and 4.
+
+Contains a main menu, gun game, platformer, and a 3D object spawner.
+
 Written and developed by Jacob Jenness and Elena Lam
